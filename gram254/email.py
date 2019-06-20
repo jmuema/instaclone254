5285d117065c0ba03c282f84994ee28571e91666
josephmuema <jmuema95@gmail.com>
2019-06-20 10:12:56 +0300

    imported mail module

---
+++ gram254/email.py
@@ -0,0 +1,5 @@
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from .token import account_activation_token
