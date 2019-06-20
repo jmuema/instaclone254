8d5c6b1a86a68143621fee88a752bb5c2453607b
josephmuema <jmuema95@gmail.com>
2019-06-20 09:35:03 +0300

    added an index functon

--- gram254/views.py
+++ gram254/views.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def index(request):
+    images = Image.acquire_all_images()
+
+    return render(request, 'home.html', {'images': images})
