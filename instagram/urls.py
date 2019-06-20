794f7e36ab711636f4eb984e84630fa7f116cd57
josephmuema <jmuema95@gmail.com>
2019-06-20 12:27:20 +0300

    added the admin path to the url pattern

--- instagram/urls.py
+++ instagram/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('gram.urls')),
+    url(r'', include('gram254.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}),
     url(r'^tinymce/', include('tinymce.urls')),
