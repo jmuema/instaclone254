99f34a021ab7dcdb7c679364d0c1ef88fd4a3a4c
josephmuema <jmuema95@gmail.com>
2019-06-20 10:02:22 +0300

    ceated a registration function

--- gram254/views.py
+++ gram254/views.py
@@ -10,3 +10,21 @@ def index(request):
     images = Image.acquire_all_images()
 
     return render(request, 'home.html', {'images': images})
+
+def registration(request):
+    if request.user.is_authenticated():
+        return redirect('index')
+    else:
+        if request.method == 'POST':
+            form = SignupForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.is_active = False
+                user.save()
+                current_site = get_current_site(request)
+                email = form.cleaned_data.get('email')
+                activation_email(user, current_site, email)
+                return HttpResponse('Complete registration by confirming your email address')
+        else:
+            form = SignupForm()
+        return render(request, 'registration/registration_form.html', {'form': form})
