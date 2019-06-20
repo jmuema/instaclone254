96c05fd8a03cb0a648bd6d36f16255182562edd5
josephmuema <jmuema95@gmail.com>
2019-06-20 10:17:37 +0300

    Added a function to confirm the password entered

--- gram254/views.py
+++ gram254/views.py
@@ -28,3 +28,21 @@ def registration(request):
         else:
             form = SignupForm()
         return render(request, 'registration/registration_form.html', {'form': form})
+
+def confirm(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+
+        return HttpResponse('Successful. Feel free to log into your account.')
+
+    else:
+        return HttpResponse('Invalid')
