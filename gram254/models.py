824a0d3acbe59ab9cf1968d2d40b2b157cf5ebea
josephmuema <jmuema95@gmail.com>
2019-06-20 09:10:53 +0300

    imported django models

--- gram254/models.py
+++ gram254/models.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from pyuploadcare.dj.models import ImageField
+from tinymce.models import HTMLField
+from django.contrib.auth.models import User
+
 
 # Create your models here.
